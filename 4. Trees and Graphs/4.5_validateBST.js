59292215445f58046b351607cdde5bee53118737
Joe Buono <joebuono724@gmail.com>
2021-03-24 18:02:14 -0400

    validateBST

--- 4. Trees and Graphs/4.5_validateBST.js
+++ 4. Trees and Graphs/4.5_validateBST.js
@@ -11,10 +11,9 @@ function TreeNode(val, left, right) {
   this.right = (right===undefined ? null : right)
 }
 
-// Solution 1: In order
 
 
-// Solution 2
+// Solution 2: (The Best)
 // Time: O(n), we must visit each node
 // Space: O(log n) on balanced tree, there are up to O(log n) recursive calls on the stack since we may recurse up to the depth of the tree
 var validateBST = function(root, min, max) {
@@ -23,3 +22,38 @@ var validateBST = function(root, min, max) {
   if (max !== null && root.val > max) return false;
   return validateBST(root.left, min, root.val) && validateBST(root.left, root.val, max);
 }
+
+// Solution 1: In-order (not fully fleshed out because it's not the most efficient)
+var validateBST = function(root) {
+  const traversal = [];
+
+  const inorderDFS = function(node) {
+    if (!node) return null;
+    inorderDFS(node.left);
+    traversal.push(node);
+    inorderDFS(node.right);
+  }
+
+  inorderDFS(root);
+};
+
+
+var iterativeInorder = function(root) {
+  const results = [];
+  const stack = [];
+
+  while (root || stack.length) {
+    if (root) {
+      stack.push(root);
+      root = root.left;
+    } else {
+      root = stack.pop();
+      results.push(root.val);
+      root = root.right;
+    }
+  }
+
+  return results;
+};
+
+
